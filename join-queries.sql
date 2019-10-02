fb25c50a16dd8d7781dc79fa2cc4dd7e2c014938
shawnxu0 <55115824+shawnxu0@users.noreply.github.com>
2019-10-02 18:16:21 -0500

    Updating join-queries

--- join-queries.sql
+++ join-queries.sql
@@ -1,27 +1,28 @@
 -- Returns movie titles and characters if the film is adult
 SELECT primaryTitle, originalTitle, characters 
-FROM `coherent-server-252621.imdb_staging.title_basics` t JOIN `coherent-server-252621.imdb_staging.title_principals` c
+FROM `coherent-server-252621.imdb_modeled.title_basics` t JOIN `coherent-server-252621.imdb_modeled.Principles` c
 ON t.tconst = c.tconst AND isAdult = 1
+
 --displays movie titles and directorsID for years after 1970
-SELECT primaryTitle, originalTitle, directors, startYear FROM `coherent-server-252621.imdb_staging.title_basics` t JOIN `coherent-server-252621.imdb_staging.title_crew` c
+SELECT primaryTitle, originalTitle, directors, startYear FROM `coherent-server-252621.imdb_modeled.title_basics` t JOIN `coherent-server-252621.imdb_modeled.Crew` c
 ON t.tconst = c.tconst AND startYear > 1970
 
--- Returns movie titles, region, and language 
-SELECT primaryTitle, originalTitle, region, language 
-FROM `coherent-server-252621.imdb_staging.title_basics` t LEFT JOIN `coherent-server-252621.imdb_staging.title_akas` c
-ON t.tconst = c.titleId
+-- Returns movie titles, region, and language for episodes
+SELECT primaryTitle, originalTitle, region, language, episodeNumber
+FROM `coherent-server-252621.imdb_modeled.title_basics` t RIGHT JOIN `coherent-server-252621.imdb_modeled.Episodes` c
+ON t.tconst = c.tconst AND c.episodeNumber is not null
 
 --Returns people, including directors, ordered by year of birth
-SELECT primaryName, birthYear
-FROM `coherent-server-252621.imdb_staging.name_basics_fix` LEFT JOIN `coherent-server-252621.imdb_staging.title_crew` on nconst = directors
+SELECT distinct primaryName, birthYear
+FROM imdb_modeled.People LEFT JOIN imdb_modeled.Crew on nconst = directors
 ORDER BY birthYear
 
 --Returns all titles including titles that have been given ratings, in alphabetical order
-SELECT title, region
-FROM `coherent-server-252621.imdb_staging.title_akas` LEFT JOIN `coherent-server-252621.imdb_staging.title_ratings` on titleID = tconst
-ORDER BY title
+SELECT distinct primaryTitle, region
+FROM imdb_modeled.title_basics as tb LEFT JOIN imdb_modeled.Ratings as r on tb.tconst = r.tconst
+ORDER BY primaryTitle
 
 --Returns the names of all people who were principal cast members on their titles, ordered by birth year
-SELECT primaryName, birthYear
-FROM `coherent-server-252621.imdb_staging.name_basics_fix` as name JOIN `coherent-server-252621.imdb_staging.title_principals` as principal on name.nconst = principal.nconst
+SELECT distinct primaryName, birthYear
+FROM imdb_modeled.People as name JOIN imdb_modeled.Principles as principal on name.nconst = principal.nconst
 ORDER BY birthYear
